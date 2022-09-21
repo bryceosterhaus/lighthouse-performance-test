c5f34a2cfdf3cd4db5f66cd3e27ebf694758e7ee
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2022-09-21 14:16:29 +0400

    Add cron task on startup

--- app/server.js
+++ app/server.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const fs = require('fs');
-let ejs = require('ejs');
+const ejs = require('ejs');
+const cron = require('node-cron');
+const {exec} = require('child_process');
 
 app.get('/', (req, res) => {
-	const dataJson = require(path.join(__dirname, '/data/main.json'));
+	const dataJson = require(path.join(__dirname, '..', '/data/main.json'));
 
 	fs.readFile(__dirname + '/index.ejs', 'utf-8', (err, html) => {
 		res.send(
@@ -21,4 +23,18 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}`);
+
+	// Runs every hour
+	cron.schedule('0 * * * *', () => {
+		exec(
+			'sh ' + path.join(__dirname, '../run.sh'),
+			(error, stdout, stderr) => {
+				console.log(stdout);
+				console.log(stderr);
+				if (error !== null) {
+					console.log(`exec error: ${error}`);
+				}
+			}
+		);
+	});
 });
